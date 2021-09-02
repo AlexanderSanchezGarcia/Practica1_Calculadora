4e1ecd0ff1689245f3bb56bd6518447ebfefdbad
AlexanderSanchezGarcia <sanchez.garcia.miguel.alexander.6@gmail.com>
2021-09-02 00:08:00 -0500

    Practica1_Calculadora

--- app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
+++ app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
@@ -1,12 +1,16 @@
 package ipn.cecyt9.practica1_calculadora;
 
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.tan;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,90 +22,113 @@ public class MainActivity extends AppCompatActivity {
 
     Double numberX, numberY, result;
     String operator;
+    Boolean filter = true;
 
     public void onClickButton0(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "0");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "0");
+        }
     }
     public void onClickButton1(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "1");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "1");
+        }
     }
     public void onClickButton2(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "2");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "2");
+        }
     }
     public void onClickButton3(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "3");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "3");
+        }
     }
     public void onClickButton4(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "4");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "4");
+        }
     }
     public void onClickButton5(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "5");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "5");
+        }
     }
     public void onClickButton6(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "6");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "6");
+        }
     }
     public void onClickButton7(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "7");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "7");
+        }
     }
     public void onClickButton8(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "8");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "8");
+        }
     }
     public void onClickButton9(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + "9");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + "9");
+        }
     }
     public void onClickButtonDot(View myView) {
-        TextView tv = findViewById(R.id.textView);
-        String value = tv.getText().toString();
-        tv.setText(value + ".");
+        if(filter = true) {
+            TextView tv = findViewById(R.id.textView1);
+            String value = tv.getText().toString();
+            tv.setText(value + ".");
+        }
     }
 
 
-    public void onClickOperatorCaptureNumberX(View myView) {
-        TextView tv = (TextView) findViewById(R.id.textView);
+    public void onClickOperatorCaptureNumber(View myView) {
+        TextView tv = (TextView) findViewById(R.id.textView1);
         numberX = Double.parseDouble(tv.getText().toString());
         tv.setText("");
     }
 
     public void onClickSum(View myView) {
         operator = "+";
-        onClickOperatorCaptureNumberX(myView);
+        onClickOperatorCaptureNumber(myView);
     }
     public void onClickMinus(View myView) {
         operator = "-";
-        onClickOperatorCaptureNumberX(myView);
+        onClickOperatorCaptureNumber(myView);
     }
     public void onClickMulti(View myView) {
         operator = "x";
-        onClickOperatorCaptureNumberX(myView);
+        onClickOperatorCaptureNumber(myView);
     }
     public void onClickDivision(View myView) {
         operator = "/";
-        onClickOperatorCaptureNumberX(myView);
+        onClickOperatorCaptureNumber(myView);
     }
 
 
     public void onClickEqual(View myView) {
-        TextView tv = (TextView) findViewById(R.id.textView);
+        TextView tv = (TextView) findViewById(R.id.textView1);
         numberY = Double.parseDouble(tv.getText().toString());
 
         try {
@@ -122,15 +149,56 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
             tv.setText(result.toString());
+            filter = false;
         } catch(NullPointerException e) {
             Toast.makeText(this,"Incorrect Number", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void onClickButtonClear(View myView) {
-        TextView tv = (TextView) findViewById(R.id.textView);
-        tv.setText("");
+    public void onClickButtonSen(View myView) {
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        try {
+            numberX = Double.parseDouble(tv.getText().toString());
+            double numberXrd = Math.toRadians(numberX);
+            result = Math.sin(numberXrd);
+            tv.setText(String.valueOf(result));
+            filter = false;
+        } catch(NullPointerException e) {
+            Toast.makeText(this,"Incorrect Number", Toast.LENGTH_SHORT).show();
+        }
+        tv.setText(result.toString());
+    }
+    public void onClickButtonCos(View myView) {
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        try {
+            numberX = Double.parseDouble(tv.getText().toString());
+            double numberXrd = Math.toRadians(numberX);
+            result = Math.sin(numberXrd);
+            tv.setText(String.valueOf(result));
+            filter = false;
+        } catch(NullPointerException e) {
+            Toast.makeText(this,"Incorrect Number", Toast.LENGTH_SHORT).show();
+        }
+        tv.setText(result.toString());
+    }
+    public void onClickButtonTan(View myView) {
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        try {
+            numberX = Double.parseDouble(tv.getText().toString());
+            double numberXrd = Math.toRadians(numberX);
+            result = Math.sin(numberXrd);
+            tv.setText(String.valueOf(result));
+            filter = false;
+        } catch(NullPointerException e) {
+            Toast.makeText(this,"Incorrect Number", Toast.LENGTH_SHORT).show();
+        }
+        tv.setText(result.toString());
     }
 
+    public void onClickButtonClear(View myView) {
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setText("");
+        filter = true;
+    }
 
 }
